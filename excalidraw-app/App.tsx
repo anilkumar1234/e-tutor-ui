174d3db41787fe48e3dec22ca788219b13ecec88
AnilKumar <anilkumar@everest.engineering>
2025-11-30 18:17:41 +0530

    remove junk 2

--- excalidraw-app/App.tsx
+++ excalidraw-app/App.tsx
@@ -1024,107 +1024,107 @@ const ExcalidrawWrapper = () => {
                 setShareDialogState({ isOpen: true, type: "share" });
               },
             },
-            {
-              label: "GitHub",
-              icon: GithubIcon,
-              category: DEFAULT_CATEGORIES.links,
-              predicate: true,
-              keywords: [
-                "issues",
-                "bugs",
-                "requests",
-                "report",
-                "features",
-                "social",
-                "community",
-              ],
-              perform: () => {
-                window.open(
-                  "https://github.com/excalidraw/excalidraw",
-                  "_blank",
-                  "noopener noreferrer",
-                );
-              },
-            },
-            {
-              label: t("labels.followUs"),
-              icon: XBrandIcon,
-              category: DEFAULT_CATEGORIES.links,
-              predicate: true,
-              keywords: ["twitter", "contact", "social", "community"],
-              perform: () => {
-                window.open(
-                  "https://x.com/excalidraw",
-                  "_blank",
-                  "noopener noreferrer",
-                );
-              },
-            },
-            {
-              label: t("labels.discordChat"),
-              category: DEFAULT_CATEGORIES.links,
-              predicate: true,
-              icon: DiscordIcon,
-              keywords: [
-                "chat",
-                "talk",
-                "contact",
-                "bugs",
-                "requests",
-                "report",
-                "feedback",
-                "suggestions",
-                "social",
-                "community",
-              ],
-              perform: () => {
-                window.open(
-                  "https://discord.gg/UexuTaE",
-                  "_blank",
-                  "noopener noreferrer",
-                );
-              },
-            },
-            {
-              label: "YouTube",
-              icon: youtubeIcon,
-              category: DEFAULT_CATEGORIES.links,
-              predicate: true,
-              keywords: ["features", "tutorials", "howto", "help", "community"],
-              perform: () => {
-                window.open(
-                  "https://youtube.com/@excalidraw",
-                  "_blank",
-                  "noopener noreferrer",
-                );
-              },
-            },
-            ...(isExcalidrawPlusSignedUser
-              ? [
-                {
-                  ...ExcalidrawPlusAppCommand,
-                  label: "Sign in / Go to Excalidraw+",
-                },
-              ]
-              : [ExcalidrawPlusCommand, ExcalidrawPlusAppCommand]),
+            // {
+            //   label: "GitHub",
+            //   icon: GithubIcon,
+            //   category: DEFAULT_CATEGORIES.links,
+            //   predicate: true,
+            //   keywords: [
+            //     "issues",
+            //     "bugs",
+            //     "requests",
+            //     "report",
+            //     "features",
+            //     "social",
+            //     "community",
+            //   ],
+            //   perform: () => {
+            //     window.open(
+            //       "https://github.com/excalidraw/excalidraw",
+            //       "_blank",
+            //       "noopener noreferrer",
+            //     );
+            //   },
+            // },
+            // {
+            //   label: t("labels.followUs"),
+            //   icon: XBrandIcon,
+            //   category: DEFAULT_CATEGORIES.links,
+            //   predicate: true,
+            //   keywords: ["twitter", "contact", "social", "community"],
+            //   perform: () => {
+            //     window.open(
+            //       "https://x.com/excalidraw",
+            //       "_blank",
+            //       "noopener noreferrer",
+            //     );
+            //   },
+            // },
+            // {
+            //   label: t("labels.discordChat"),
+            //   category: DEFAULT_CATEGORIES.links,
+            //   predicate: true,
+            //   icon: DiscordIcon,
+            //   keywords: [
+            //     "chat",
+            //     "talk",
+            //     "contact",
+            //     "bugs",
+            //     "requests",
+            //     "report",
+            //     "feedback",
+            //     "suggestions",
+            //     "social",
+            //     "community",
+            //   ],
+            //   perform: () => {
+            //     window.open(
+            //       "https://discord.gg/UexuTaE",
+            //       "_blank",
+            //       "noopener noreferrer",
+            //     );
+            //   },
+            // },
+            // {
+            //   label: "YouTube",
+            //   icon: youtubeIcon,
+            //   category: DEFAULT_CATEGORIES.links,
+            //   predicate: true,
+            //   keywords: ["features", "tutorials", "howto", "help", "community"],
+            //   perform: () => {
+            //     window.open(
+            //       "https://youtube.com/@excalidraw",
+            //       "_blank",
+            //       "noopener noreferrer",
+            //     );
+            //   },
+            // },
+            // ...(isExcalidrawPlusSignedUser
+            //   ? [
+            //     {
+            //       ...ExcalidrawPlusAppCommand,
+            //       label: "Sign in / Go to Excalidraw+",
+            //     },
+            //   ]
+            //   : [ExcalidrawPlusCommand, ExcalidrawPlusAppCommand]),
 
-            {
-              label: t("overwriteConfirm.action.excalidrawPlus.button"),
-              category: DEFAULT_CATEGORIES.export,
-              icon: exportToPlus,
-              predicate: true,
-              keywords: ["plus", "export", "save", "backup"],
-              perform: () => {
-                if (excalidrawAPI) {
-                  exportToExcalidrawPlus(
-                    excalidrawAPI.getSceneElements(),
-                    excalidrawAPI.getAppState(),
-                    excalidrawAPI.getFiles(),
-                    excalidrawAPI.getName(),
-                  );
-                }
-              },
-            },
+            // {
+            //   label: t("overwriteConfirm.action.excalidrawPlus.button"),
+            //   category: DEFAULT_CATEGORIES.export,
+            //   icon: exportToPlus,
+            //   predicate: true,
+            //   keywords: ["plus", "export", "save", "backup"],
+            //   perform: () => {
+            //     if (excalidrawAPI) {
+            //       exportToExcalidrawPlus(
+            //         excalidrawAPI.getSceneElements(),
+            //         excalidrawAPI.getAppState(),
+            //         excalidrawAPI.getFiles(),
+            //         excalidrawAPI.getName(),
+            //       );
+            //     }
+            //   },
+            // },
             {
               ...CommandPalette.defaultItems.toggleTheme,
               perform: () => {
--- packages/excalidraw/components/LayerUI.tsx
+++ packages/excalidraw/components/LayerUI.tsx
@@ -119,9 +119,9 @@ const DefaultMainMenu: React.FC<{
       <MainMenu.DefaultItems.Help />
       <MainMenu.DefaultItems.ClearCanvas />
       <MainMenu.Separator />
-      <MainMenu.Group title="Excalidraw links">
+      {/* <MainMenu.Group title="Excalidraw links">
         <MainMenu.DefaultItems.Socials />
-      </MainMenu.Group>
+      </MainMenu.Group> */}
       <MainMenu.Separator />
       <MainMenu.DefaultItems.ToggleTheme />
       <MainMenu.DefaultItems.ChangeCanvasBackground />
@@ -167,21 +167,21 @@ const LayerUI = ({
 
   const spacing = isCompactStylesPanel
     ? {
-        menuTopGap: 4,
-        toolbarColGap: 4,
-        toolbarRowGap: 1,
-        toolbarInnerRowGap: 0.5,
-        islandPadding: 1,
-        collabMarginLeft: 8,
-      }
+      menuTopGap: 4,
+      toolbarColGap: 4,
+      toolbarRowGap: 1,
+      toolbarInnerRowGap: 0.5,
+      islandPadding: 1,
+      collabMarginLeft: 8,
+    }
     : {
-        menuTopGap: 6,
-        toolbarColGap: 4,
-        toolbarRowGap: 1,
-        toolbarInnerRowGap: 1,
-        islandPadding: 1,
-        collabMarginLeft: 8,
-      };
+      menuTopGap: 6,
+      toolbarColGap: 4,
+      toolbarRowGap: 1,
+      toolbarInnerRowGap: 1,
+      islandPadding: 1,
+      collabMarginLeft: 8,
+    };
 
   const TunnelsJotaiProvider = tunnels.tunnelsJotai.Provider;
 
@@ -449,8 +449,7 @@ const LayerUI = ({
           trackEvent(
             "sidebar",
             `toggleDock (${docked ? "dock" : "undock"})`,
-            `(${
-              editorInterface.formFactor === "phone" ? "mobile" : "desktop"
+            `(${editorInterface.formFactor === "phone" ? "mobile" : "desktop"
             })`,
           );
         }}
@@ -479,8 +478,7 @@ const LayerUI = ({
             trackEvent(
               "sidebar",
               `${DEFAULT_SIDEBAR.name} (open)`,
-              `button (${
-                editorInterface.formFactor === "phone" ? "mobile" : "desktop"
+              `button (${editorInterface.formFactor === "phone" ? "mobile" : "desktop"
               })`,
             );
           }
@@ -519,8 +517,8 @@ const LayerUI = ({
                       ? "strokeColor"
                       : "backgroundColor"
                     : colorPickerType === "elementBackground"
-                    ? "backgroundColor"
-                    : "strokeColor"]: color,
+                      ? "backgroundColor"
+                      : "strokeColor"]: color,
                 });
                 ShapeCache.delete(element);
               }
@@ -600,8 +598,8 @@ const LayerUI = ({
             className="layer-ui__wrapper"
             style={
               appState.openSidebar &&
-              isSidebarDocked &&
-              editorInterface.canFitSidebar
+                isSidebarDocked &&
+                editorInterface.canFitSidebar
                 ? { width: `calc(100% - var(--right-sidebar-width))` }
                 : {}
             }
